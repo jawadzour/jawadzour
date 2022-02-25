e3af178f0eecd9f3f6fcd8ac2fa80455f3cd42d1
Jawad Nabi <jawad.nabi@systemsltd.com>
2022-02-25 19:57:56 +0500

    Addition

--- app.js
+++ app.js
@@ -2,20 +2,28 @@
   'use strict';
 
   angular.module("myFirstApp",[])
-  .controller("MyController",function($scope){
-    $scope.name="Jawad Nabi";
-    $scope.finalValue=0;
-    $scope.displayNumber=function(){
-      var totalValue=calculateValue($scope.name);
-      $scope.finalValue=totalValue;
-    }
+  .controller("MyController",MyController);
+    function MyController($scope,$filter,$injector){
+      $scope.name="Jawad Nabi";
 
-    function calculateValue(string){
-      var totalStringValue=0;
-      for(var i=0; i<string.length; i++){
-          totalStringValue+=string.charCodeAt(i);
+      $scope.upper=function(){
+        var upCase=$filter('uppercase');
+        $scope.name=upCase($scope.name);
       }
-      return totalStringValue;
+      console.log($injector.annotate(MyController));
     }
-  });
+  //   $scope.finalValue=0;
+  //   $scope.displayNumber=function(){
+  //     var totalValue=calculateValue($scope.name);
+  //     $scope.finalValue=totalValue;
+  //   }
+  //
+  //   function calculateValue(string){
+  //     var totalStringValue=0;
+  //     for(var i=0; i<string.length; i++){
+  //         totalStringValue+=string.charCodeAt(i);
+  //     }
+  //     return totalStringValue;
+  //   }
+  // });
 })();
